4a403f15d56e73ee2a8b618b973b38f313913680
GotPop <gotpop@users.noreply.github.com>
2026-04-14 13:55:20 +0100

    Remove unused files

--- template-parts/content/homepage.php
+++
@@ -1,5 +0,0 @@
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div id="post-<?php the_ID(); ?>" <?php post_class( 'homepage-content' ); ?>>
-		<?php the_content(); ?>
-	</div>
-<?php endwhile; endif; ?>
