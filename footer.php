dbc74caf607e128fcd2a1a8162f389a7f2e009b1
GotPop <gotpop@users.noreply.github.com>
2026-04-14 13:07:12 +0100

    Formatting

--- footer.php
+++ footer.php
@@ -1,12 +1,6 @@
-</main>
-<?php get_sidebar(); ?>
-</div>
 <footer id="footer" role="contentinfo">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'observata' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
+    <div id="copyright">
+        &copy; <?php echo esc_html( date_i18n( __( 'Y', 'observata' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+    </div>
 </footer>
-</div>
 <?php wp_footer(); ?>
-</body>
-</html>
--- template-parts/footers/homepage.php
+++
@@ -1,5 +0,0 @@
-<footer id="footer" class="footer-homepage" role="contentinfo">
-	<div id="copyright">
-		&copy; <?php echo esc_html( date_i18n( __( 'Y', 'observata' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-	</div>
-</footer>
