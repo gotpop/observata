c53cedecec962166db45b9df995b93f38b8d8ed9
GotPop <gotpop@users.noreply.github.com>
2026-04-14 13:00:25 +0100

    Deleting blog files

--- nav-below.php
+++
@@ -1,5 +0,0 @@
-<?php $args = array(
-'prev_text' => sprintf( esc_html__( '%s older', 'observata' ), '<span class="meta-nav">&larr;</span>' ),
-'next_text' => sprintf( esc_html__( 'newer %s', 'observata' ), '<span class="meta-nav">&rarr;</span>' )
-);
-the_posts_navigation( $args );
