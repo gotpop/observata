07e5abb280425d5a198261bc37f1a686aeec28d7
GotPop <gotpop@users.noreply.github.com>
2026-04-30 12:27:05 +0100

    Refactor templates to use header and footer variables instead of do_blocks function

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -6,11 +6,13 @@
  * Set that page as the front page under Settings → Reading → "A static page".
  */
 
-$context               = \Timber\Timber::context();
-$post_obj              = \Timber\Timber::get_post();
+$context = \Timber\Timber::context();
+$post_obj = \Timber\Timber::get_post();
 $post_obj->setup();
-$context['post']       = $post_obj;
-$context['content']    = apply_filters( 'the_content', get_the_content() );
-$context['body_class'] = implode( ' ', get_body_class( 'has-homepage-header' ) );
+$context['post'] = $post_obj;
+$context['content'] = apply_filters('the_content', get_the_content());
+$context['body_class'] = implode(' ', get_body_class('has-homepage-header'));
+$context['header'] = do_blocks('<!-- wp:observata/header /-->');
+$context['footer'] = do_blocks('<!-- wp:observata/footer /-->');
 
-\Timber\Timber::render( 'templates/page-home.twig', $context );
+\Timber\Timber::render('templates/page-home.twig', $context);
--- page-templates/page-default.php
+++ page-templates/page-default.php
@@ -3,11 +3,13 @@
  * Template Name: Default Page
  */
 
-$context               = \Timber\Timber::context();
-$post_obj              = \Timber\Timber::get_post();
+$context = \Timber\Timber::context();
+$post_obj = \Timber\Timber::get_post();
 $post_obj->setup();
-$context['post']       = $post_obj;
-$context['content']    = apply_filters( 'the_content', get_the_content() );
-$context['body_class'] = implode( ' ', get_body_class() );
+$context['post'] = $post_obj;
+$context['content'] = apply_filters('the_content', get_the_content());
+$context['body_class'] = implode(' ', get_body_class());
+$context['header'] = do_blocks('<!-- wp:observata/header /-->');
+$context['footer'] = do_blocks('<!-- wp:observata/footer /-->');
 
-\Timber\Timber::render( 'templates/page-default.twig', $context );
+\Timber\Timber::render('templates/page-default.twig', $context);
