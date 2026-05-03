244c35327a2e82efb76a395c00c1e0374ccdf687
GotPop <gotpop@users.noreply.github.com>
2026-05-03 12:00:44 +0100

    Refactor card-simple block

--- src/card-simple/edit.jsx
+++ src/card-simple/edit.jsx
@@ -1,11 +1,11 @@
 import './editor.css';
 
-import { MediaUpload, MediaUploadCheck, RichText, useBlockProps } from '@wordpress/block-editor';
-import { Button, PanelBody, SelectControl, TextControl } from '@wordpress/components';
+import { RichText, useBlockProps } from '@wordpress/block-editor';
 
-import { InspectorControls } from '@wordpress/block-editor';
+import { SelectControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import BlockLabel from '../components/block-label';
+import GeoIcon from '../components/geo-icon';
 
 const iconOptions = Array.from({ length: 30 }, (_, i) => {
     const num = String(i + 1).padStart(2, '0');
@@ -13,77 +13,38 @@ const iconOptions = Array.from({ length: 30 }, (_, i) => {
 });
 
 export default function Edit({ attributes, setAttributes }) {
-    const { imageUrl, imageAlt, title, description, iconGeo } = attributes;
-    const blockProps = useBlockProps();
+    const { title, description, iconGeo } = attributes;
+    const blockProps = useBlockProps({ className: 'card-simple' });
 
     return (
-        <>
-            <InspectorControls>
-                <PanelBody title={__('Image Settings', 'observata')}>
-                    <MediaUploadCheck>
-                        <MediaUpload
-                            onSelect={(media) => setAttributes({ imageUrl: media.url, imageAlt: media.alt })}
-                            allowedTypes={['image']}
-                            render={({ open }) => (
-                                <Button variant="secondary" onClick={open}>
-                                    {imageUrl ? __('Replace Image', 'observata') : __('Select Image', 'observata')}
-                                </Button>
-                            )}
-                        />
-                    </MediaUploadCheck>
-                    {imageUrl && (
-                        <Button
-                            variant="link"
-                            isDestructive
-                            onClick={() => setAttributes({ imageUrl: '', imageAlt: '' })}
-                        >
-                            {__('Remove Image', 'observata')}
-                        </Button>
-                    )}
-                    <TextControl
-                        label={__('Image Alt Text', 'observata')}
-                        value={imageAlt}
-                        onChange={(val) => setAttributes({ imageAlt: val })}
-                    />
-                </PanelBody>
-                <PanelBody title={__('Icon Settings', 'observata')}>
-                    <SelectControl
-                        label={__('Geo Icon', 'observata')}
-                        value={iconGeo}
-                        options={[{ label: __('None', 'observata'), value: '' }, ...iconOptions]}
-                        onChange={(val) => setAttributes({ iconGeo: val })}
-                    />
-                </PanelBody>
-            </InspectorControls>
+        <article {...blockProps}>
+            <BlockLabel name="Card Simple" />
 
-            <div {...blockProps}>
-                <BlockLabel name="Card Simple" />
-
-                <div className="card-simple">
-                    {iconGeo && (
-                        <div className="card-simple__icon is-placeholder">
-                            <span className="icon-geo">{iconGeo}</span>
-                        </div>
-                    )}
-                    {!iconGeo && imageUrl && (
-                        <img className="card-simple__image" src={imageUrl} alt={imageAlt} />
-                    )}
-                    <RichText
-                        tagName="h3"
-                        className="card-simple__title"
-                        value={title}
-                        onChange={(val) => setAttributes({ title: val })}
-                        placeholder={__('Card title…', 'observata')}
-                    />
-                    <RichText
-                        tagName="p"
-                        className="card-simple__description"
-                        value={description}
-                        onChange={(val) => setAttributes({ description: val })}
-                        placeholder={__('Card description…', 'observata')}
-                    />
-                </div>
+            <div className="intro-card-icon">
+                <GeoIcon number={iconGeo} />
             </div>
-        </>
+            <div className="intro-card-body">
+                <RichText
+                    tagName="h3"
+                    className="card-simple__title"
+                    value={title}
+                    onChange={(val) => setAttributes({ title: val })}
+                    placeholder={__('Card title…', 'observata')}
+                />
+                <RichText
+                    tagName="p"
+                    className="card-simple__description"
+                    value={description}
+                    onChange={(val) => setAttributes({ description: val })}
+                    placeholder={__('Card description…', 'observata')}
+                />
+                <SelectControl
+                    label={__('Icon', 'observata')}
+                    value={iconGeo}
+                    options={iconOptions}
+                    onChange={(val) => setAttributes({ iconGeo: val })}
+                />
+            </div>
+        </article>
     );
 }
