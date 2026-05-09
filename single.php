1a6b49fd7f1d53c8ee012295e493a1c0470f5d84
GotPop <gotpop@users.noreply.github.com>
2026-05-09 22:06:36 +0100

    feat: separate hero block and wrap body content
    - Parse blocks in single.php to extract the hero section from the rest of the body content.
    - Update Twig template to render the hero outside of the newly added `<content-blog>` wrapper.
    - Add CSS grid properties for the `.content-blog` class to handle layout.
    - Unignore `.air` configuration in `.gitignore`.

--- single.php
+++ single.php
@@ -4,7 +4,20 @@ $context = \Timber\Timber::context();
 $post_obj = \Timber\Timber::get_post();
 $post_obj->setup();
 $context['post'] = $post_obj;
-$context['content'] = apply_filters('the_content', get_the_content());
+$blocks = parse_blocks(get_the_content());
+$hero_content = '';
+$body_content = '';
+
+foreach ($blocks as $block) {
+    if ($block['blockName'] === 'observata/hero-blog') {
+        $hero_content .= render_block($block);
+    } else {
+        $body_content .= render_block($block);
+    }
+}
+
+$context['hero'] = $hero_content;
+$context['content'] = $body_content;
 $context['body_class'] = implode(' ', get_body_class());
 $context['header'] = do_blocks('<!-- wp:observata/header /-->');
 $context['footer'] = do_blocks('<!-- wp:observata/footer /-->');
