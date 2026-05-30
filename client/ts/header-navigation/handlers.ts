184e8a299e68cfc93d4b8baffe0774fe8be15766
GotPop <gotpop@users.noreply.github.com>
2026-05-30 11:10:32 +0100

    Refactor ts file structure

---
+++ client/ts/header-navigation/handlers.ts
@@ -0,0 +1,42 @@
+export function handleTriggerClick(trigger: HTMLElement | null, headerContent: HTMLElement | null): void {
+	if (!trigger || !headerContent) return;
+
+	const isOpen = headerContent.classList.toggle('is-open');
+
+	trigger.setAttribute('aria-expanded', String(isOpen));
+	const label = trigger.querySelector('.sr-only');
+
+	if (label) {
+		label.textContent = isOpen ? 'Close menu' : 'Open menu';
+	}
+
+	const hasOpenClass = headerContent.classList.contains('is-open');
+	const iconHamburger = document.getElementById('icon-hamburger');
+
+	if (iconHamburger) {
+		iconHamburger.classList.toggle('is-active', hasOpenClass);
+	}
+}
+
+export function handleSubmenuEnter(e: Event): void {
+	const parent = e.currentTarget as HTMLElement;
+	const button = parent.querySelector('.menu-button') as HTMLElement;
+
+	if (button) button.setAttribute('aria-expanded', 'true');
+}
+
+export function handleSubmenuLeave(e: Event): void {
+	const parent = e.currentTarget as HTMLElement;
+	const button = parent.querySelector('.menu-button') as HTMLElement;
+
+	if (button) button.setAttribute('aria-expanded', 'false');
+}
+
+export function handleSubmenuClick(e: Event): void {
+	e.preventDefault();
+
+	const el = e.currentTarget as HTMLElement;
+	const isExpanded = el.getAttribute('aria-expanded') === 'true';
+
+	el.setAttribute('aria-expanded', String(!isExpanded));
+}
--- client/ts/header-navigation/index.ts
+++ client/ts/header-navigation/index.ts
@@ -1,1 +1,36 @@
-export { initHeaderNavigation } from './header-navigation';
+import { handleSubmenuClick, handleSubmenuEnter, handleSubmenuLeave, handleTriggerClick } from './handlers';
+
+import { createMatchMedia } from '../utils';
+
+export function initHeaderNavigation(): void {
+	const trigger = document.getElementById('trigger-navigation');
+	const headerContent = document.getElementById('header-content');
+	const parentMenuItems = document.querySelectorAll('.menu-item-has-children');
+
+	const mq = createMatchMedia('sm');
+
+	if (trigger && headerContent) {
+		trigger.addEventListener('click', () => handleTriggerClick(trigger, headerContent));
+	}
+
+	function attachListeners() {
+		parentMenuItems.forEach((parentItem) => {
+			const button = parentItem.querySelector('.menu-button') as HTMLElement;
+
+			if (mq.matches) {
+				parentItem.addEventListener('mouseenter', handleSubmenuEnter);
+				parentItem.addEventListener('mouseleave', handleSubmenuLeave);
+
+				if (button) button.removeEventListener('click', handleSubmenuClick);
+			} else {
+				parentItem.removeEventListener('mouseenter', handleSubmenuEnter);
+				parentItem.removeEventListener('mouseleave', handleSubmenuLeave);
+
+				if (button) button.addEventListener('click', handleSubmenuClick);
+			}
+		});
+	}
+
+	attachListeners();
+	mq.addEventListener('change', attachListeners);
+}
