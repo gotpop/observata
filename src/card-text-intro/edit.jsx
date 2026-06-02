042b3c7e8e9c78d1f63f4eb6336981bb51515320
GotPop <gotpop@users.noreply.github.com>
2026-06-02 10:20:57 +0100

    Updating table blocks

--- src/card-text-intro/edit.jsx
+++ src/card-text-intro/edit.jsx
@@ -2,9 +2,9 @@ import './editor.css';
 
 import { InnerBlocks, RichText, useBlockProps } from '@wordpress/block-editor';
 
-import BlockLabel from '../components/block-label';
 import { SelectControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import BlockLabel from '../components/block-label';
 
 const INTRO_TEMPLATE = [
     ['observata/element-body-md', { content: 'Intro text description here.' }],
