ad4587cecd4b11b1da61fd987666247a1af644cd
GotPop <gotpop@users.noreply.github.com>
2026-06-08 20:04:08 +0100

    Broswer detect for shaders

--- client/ts/shaders/index.ts
+++ client/ts/shaders/index.ts
@@ -1,4 +1,5 @@
-import { createMatchMedia } from '../utils';
+import { createMatchMedia, isChrome } from '../utils';
+
 import { initCardGeoShader } from './card-geo-shader';
 import { initHeroShaders } from './home';
 import { initSubpageShaders } from './subpage';
@@ -11,18 +12,22 @@ export function initShaders(): void {
 	}
 
 	const heroCanvas = document.getElementById('hero-shader');
+
 	if (heroCanvas) {
 		void initHeroShaders();
 	}
 
 	const subpageCanvas = document.querySelector('.subpage-shader');
+
 	if (subpageCanvas) {
 		void initSubpageShaders();
 	}
 
-	const canvases = document.querySelectorAll<HTMLCanvasElement>('.card-geo-shader canvas');
+	if (isChrome()) {
+		const canvases = document.querySelectorAll<HTMLCanvasElement>('.card-geo-shader canvas');
 
-	for (const canvas of canvases) {
-		void initCardGeoShader(canvas);
+		for (const canvas of canvases) {
+			void initCardGeoShader(canvas);
+		}
 	}
 }
--- client/ts/utils/index.ts
+++ client/ts/utils/index.ts
@@ -1,2 +1,12 @@
 export { createMatchMedia } from './breakpoints';
+export {
+	isBrave,
+	isChrome,
+	isDesktop,
+	isEdge,
+	isFirefox,
+	isMobile,
+	isOpera,
+	isSafari,
+} from './browser';
 export { removeClickOutsideClose, setupClickOutsideClose } from './click-outside-close';
