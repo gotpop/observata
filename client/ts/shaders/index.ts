ec720ba1243260cb3bb387097b29cc60c1929eb5
GotPop <gotpop@users.noreply.github.com>
2026-06-12 16:44:38 +0100

    Disable mobile fallback

--- client/ts/shaders/index.ts
+++ client/ts/shaders/index.ts
@@ -1,14 +1,14 @@
-import { createMatchMedia } from '../utils';
+// import { createMatchMedia } from '../utils';
 import { initCardGeoShader } from './card-geo-shader';
 import { initHeroShaders } from './home';
 import { initSubpageShaders } from './subpage';
 
 export function initShaders(): void {
-	const mq = createMatchMedia('md');
+	// const mq = createMatchMedia('md');
 
-	if (!mq.matches) {
-		return;
-	}
+	// if (!mq.matches) {
+	// 	return;
+	// }
 
 	const heroCanvas = document.getElementById('hero-shader');
 
