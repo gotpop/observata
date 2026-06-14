faff92bf402209018a607a3625935d4001cea79b
GotPop <gotpop@users.noreply.github.com>
2026-06-14 16:02:40 +0100

    Removing uused group

--- client/ts/shaders/home.ts
+++ client/ts/shaders/home.ts
@@ -4,62 +4,56 @@ import { prepareCanvas } from './utils-shader/warnings';
 const shaderConfig = {
 	components: [
 		{
-			type: 'Group',
-			id: 'idmqcf2qz4cr8k2pb3i',
+			type: 'Form3D',
+			id: 'idmmr8zyxrodm90feqn',
+			props: {
+				center: {
+					x: 0.49,
+					y: 0.5,
+				},
+				glossiness: 200,
+				lighting: 197,
+				shape3d: {
+					type: 'ribbon',
+					angle: 179.5,
+					twist: 45,
+					width: 39,
+					thickness: 8,
+					seed: 33,
+				},
+				shape3dType: 'ribbon',
+				speed: 0,
+				uvMode: 'mirror',
+				zoom: 120,
+			},
 			children: [
 				{
-					type: 'Form3D',
-					id: 'idmmr8zyxrodm90feqn',
+					type: 'Swirl',
+					id: 'idmmr8zwtuhz62buy44',
 					props: {
-						center: {
-							x: 0.49,
-							y: 0.5,
-						},
-						glossiness: 200,
-						lighting: 197,
-						shape3d: {
-							type: 'ribbon',
-							angle: 179.5,
-							twist: 45,
-							width: 39,
-							thickness: 8,
-							seed: 33,
-						},
-						shape3dType: 'ribbon',
-						speed: 0,
-						uvMode: 'mirror',
-						zoom: 120,
+						colorA: '#0598ce',
+						colorB: '#133868',
+						colorSpace: 'oklab',
+						detail: 1.9,
+						visible: true,
 					},
-					children: [
-						{
-							type: 'Swirl',
-							id: 'idmmr8zwtuhz62buy44',
-							props: {
-								colorA: '#0598ce',
-								colorB: '#133868',
-								colorSpace: 'oklab',
-								detail: 1.9,
-								visible: true,
-							},
+				},
+				{
+					type: 'FallingLines',
+					id: 'idmq9cbx8wqhsm96pxv',
+					props: {
+						angle: 0,
+						blendMode: 'linearDodge',
+						density: 19,
+						opacity: 0.43,
+						speed: 0.05,
+						strokeWidth: 0.18,
+						trailLength: 0.58,
+						transform: {
+							scale: 0.95,
+							anchorX: 1,
 						},
-						{
-							type: 'FallingLines',
-							id: 'idmq9cbx8wqhsm96pxv',
-							props: {
-								angle: 0,
-								blendMode: 'linearDodge',
-								density: 19,
-								opacity: 0.43,
-								speed: 0.05,
-								strokeWidth: 0.18,
-								trailLength: 0.58,
-								transform: {
-									scale: 0.95,
-									anchorX: 1,
-								},
-							},
-						},
-					],
+					},
 				},
 			],
 		},
