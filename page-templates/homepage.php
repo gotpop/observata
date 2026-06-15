9268e986864f1da8a818cabad503f0cd6c8dee59
GotPop <gotpop@users.noreply.github.com>
2026-06-15 14:53:57 +0100

    Adding main content wrapppers

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -10,7 +10,21 @@ $context  = \Timber\Timber::context();
 $post_obj = \Timber\Timber::get_post();
 $post_obj->setup();
 $context['post']       = $post_obj;
-$context['content']    = apply_filters( 'the_content', get_the_content() );
+$blocks                = parse_blocks( get_the_content() );
+$hero_content          = '';
+$body_content          = '';
+
+// Split the hero block from the rest so non-hero content can be wrapped.
+foreach ( $blocks as $block ) {
+	if ( $block['blockName'] === 'observata/section-hero-home' ) {
+		$hero_content .= render_block( $block );
+	} else {
+		$body_content .= render_block( $block );
+	}
+}
+
+$context['hero']       = $hero_content;
+$context['content']    = $body_content;
 $context['body_class'] = implode( ' ', get_body_class( 'has-homepage-header' ) );
 $context['header']     = do_blocks( '<!-- wp:observata/header /-->' );
 $context['footer']     = do_blocks( '<!-- wp:observata/footer /-->' );
