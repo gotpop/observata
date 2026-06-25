47e4e71a2b8d50b1557b2d241dad282c8ab80d7e
GotPop <gotpop@users.noreply.github.com>
2026-06-25 19:02:16 +0100

    Make Timber context consistent

--- attachment.php
+++ attachment.php
@@ -1,26 +1,26 @@
 <?php
 
-$context     = \Timber\Timber::context();
-$timber_post = \Timber\Timber::get_post();
+$context  = \Timber\Timber::context();
+$post_obj = \Timber\Timber::get_post();
 
-if ( ! $timber_post ) {
+if ( ! $post_obj ) {
 	wp_safe_redirect( home_url( '/' ) );
 	exit;
 }
 
-$timber_post->setup();
-$context['post']                = $timber_post;
+$post_obj->setup();
+$context['post']                = $post_obj;
 $context['body_class']          = implode( ' ', get_body_class() );
-$context['is_image_attachment'] = wp_attachment_is_image( $timber_post->ID );
-$context['attachment_url']      = wp_get_attachment_url( $timber_post->ID );
+$context['is_image_attachment'] = wp_attachment_is_image( $post_obj->ID );
+$context['attachment_url']      = wp_get_attachment_url( $post_obj->ID );
 
-if ( $timber_post->post_parent ) {
-	$context['parent_permalink'] = get_permalink( $timber_post->post_parent );
-	$context['parent_title']     = get_the_title( $timber_post->post_parent );
+if ( $post_obj->post_parent ) {
+	$context['parent_permalink'] = get_permalink( $post_obj->post_parent );
+	$context['parent_title']     = get_the_title( $post_obj->post_parent );
 }
 
 if ( $context['is_image_attachment'] ) {
-	$context['attachment_image'] = wp_get_attachment_image_src( $timber_post->ID, 'full' );
+	$context['attachment_image'] = wp_get_attachment_image_src( $post_obj->ID, 'full' );
 }
 
 \Timber\Timber::render( 'templates/blog-attachment.twig', $context );
