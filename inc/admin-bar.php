2e88ae1763daa6a852d4822d1e9131bf06e50521
GotPop <gotpop@users.noreply.github.com>
2026-07-09 16:04:29 +0100

    Admin bar

--- inc/admin-bar.php
+++ inc/admin-bar.php
@@ -5,15 +5,18 @@ add_filter( 'show_admin_bar', '__return_false' );
 
 // Replace "Howdy," with a friendlier greeting in the admin bar.
 add_filter( 'gettext', 'observata_replace_howdy', 10, 3 );
+
 function observata_replace_howdy( $translated, $text, $domain ) {
 	if ( 'default' === $domain && false !== strpos( $text, 'Howdy' ) ) {
 		$translated = str_replace( 'Howdy', 'Hej', $text );
 	}
+	
 	return $translated;
 }
 
 // Enqueue admin stylesheet.
 add_action( 'admin_enqueue_scripts', 'observata_admin_styles' );
+
 function observata_admin_styles() {
 	wp_enqueue_style(
 		'observata-admin',
